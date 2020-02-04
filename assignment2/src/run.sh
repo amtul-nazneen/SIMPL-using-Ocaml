0e938f4c7c8313e91d28c156a27594217c6ab25f
amtul-nazneen <amtulnazneen7@gmail.com>
2020-02-03 20:56:24 -0600

    remove a2

--- assignment2/src/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ocamlc -c simpltypes.ml
-ocamlyacc simplparser.mly
-ocamlc -c simplparser.mli
-ocamlc -c simplparser.ml
-ocamllex simpllexer.mll
-ocamlc -c simpllexer.ml
-ocamlc -c nazneen.ml
-ocamlc -o simpl simpltypes.cmo simpllexer.cmo simplparser.cmo nazneen.cmo
